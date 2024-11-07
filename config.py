7050c0f01099f4f5cb971299f0a5bd5c1aeddc40
shijingya <1972614243@qq.com>
2024-11-07 20:55:16 +0800

    oktest

---
+++ config.py
@@ -0,0 +1,10 @@
+# 存放被测试项目基本信息，如URL
+
+import os
+
+# 设置项目环境域名
+BASE_URL = "http://kdtx-test.itheima.net"
+
+# 定义项目文件根路径
+BASE_PATH = os.path.dirname(__file__)  # 当前文件
+print(BASE_PATH)  # D:\python_learn\KDTX
